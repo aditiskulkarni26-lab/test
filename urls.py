26ddeca5777932dc8947e9522ae38cdfd4e4193c
aditiskulkarni26-lab <aditiskulkarni26@gmail.com>
2026-01-09 07:55:20 +0530

    add above files

--- urls.py
+++ urls.py
@@ -1,14 +1,16 @@
-"""from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', views.plan_list, name='customer-list'),
-    path('<int:pk>/', views.plan_detail, name='plan-detail'),
-]"""
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.plan_list, name='plan-list'),  # Corrected name
-    path('<int:pk>/', views.plan_detail, name='plan-detail'),
+    path('admin/', admin.site.urls),
+    #path('api/', include('rest_api.urls')),
+    path("customer/",include('customer.urls')),
+    path("user/",include('user.urls')),
+    path("plan/",include('plan.urls')),
+    path("investment_table/",include('investment_table.urls')),
+    path("investment_stocks/",include('investment_stocks.urls')),
+    path("investment_mutual_fund/",include('investment_mutual_fund.urls')),
+    path("investment_fd/",include('investment_fd.urls')),
+    path("investment_commodity/",include('investment_commodity.urls')),
 ]
